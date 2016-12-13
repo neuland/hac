34949582371161e7c593e890f92dc87492a8d2e7
Sebastian Korten <Sebastian.Korten@neuland-bfi.de>
2016-12-13 14:01:40 +0100

    setup.sh: don't overwrite existing config

--- setup.sh
+++ setup.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 
 BASE_DIR=~/.hac
-REPO_DIR=$(pwd)
+REPO_DIR=$(pwd)/src
 BIN_DIR=/usr/local/bin
 COMPLETION_DIR=/etc/bash_completion.d
 
-[[ -d $BASE_DIR ]] || mkdir $BASE_DIR
+COMPLETION_FILE=hac-completion.sh
+GROOVY_FILE=hac.groovy
+SHELL_WRAPPER=hac.sh
+CONFIG_FILE=config.json
 
-ln -s $REPO_DIR/src/hac-completion.sh $BASE_DIR
-ln -s $REPO_DIR/src/hac.sh $BASE_DIR
-ln -s $REPO_DIR/src/hac.groovy $BASE_DIR
-cp $REPO_DIR/src/config.json $BASE_DIR
+[[ -d "$BASE_DIR" ]] || mkdir $BASE_DIR
 
-sudo ln -s $BASE_DIR/hac-completion.sh $COMPLETION_DIR
-sudo ln -s $BASE_DIR/hac.sh $BIN_DIR/hac
+[[ -L "$BASE_DIR/$COMPLETION_FILE" ]] || ln -s "$REPO_DIR/$COMPLETION_FILE" "$BASE_DIR"
+[[ -L "$BASE_DIR/$SHELL_WRAPPER" ]]   || ln -s "$REPO_DIR/$SHELL_WRAPPER" "$BASE_DIR"
+[[ -L "$BASE_DIR/$GROOVY_FILE" ]]     || ln -s "$REPO_DIR/$GROOVY_FILE" "$BASE_DIR"
+[[ -f "$BASE_DIR/$CONFIG_FILE" ]]     || cp "$REPO_DIR/$CONFIG_FILE" "$BASE_DIR"
+
+[[ -L "$COMPLETION_DIR/$COMPLETION_FILE" ]] || sudo ln -s "$BASE_DIR/$COMPLETION_FILE" "$COMPLETION_DIR"
+[[ -L "$BIN_DIR/hac" ]]                     || sudo ln -s "$BASE_DIR/$SHELL_WRAPPER" "$BIN_DIR/hac"
 
 echo "Please source the following config file to use completion in this session."
-echo "> 'source $COMPLETION_DIR/hac-completion.sh'"
+echo "> 'source $COMPLETION_DIR/$COMPLETION_FILE'"
 echo "Completion will work automatically after restart."
