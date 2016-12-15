3c38578f0b78b7e89ffa5eff9e57f1a295d695c2
Achim Grimm <achim.grimm@neuland-bfi.de>
2016-12-15 08:43:35 +0100

    work with streams

--- src/hac.sh
+++ src/hac.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
-groovy ~/.hac/hac.groovy --env $1 --file $2
+
+currentLang=$LANG
+unset LANG
+tmpFile=`cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32`
+export LANG=$currentLang
+
+if [[ $# -eq 3 ]]; then
+    mkdir -p ~/.hac/tmp
+    cat - > ~/.hac/tmp/$tmpFile
+    exec </dev/tty >/dev/tty
+    groovy ~/.hac/hac.groovy --env $1 --file ~/.hac/tmp/$tmpFile $2 $3
+else
+    groovy ~/.hac/hac.groovy --env $1 --file $2
+fi
+
+rm -f ~/.hac/tmp/$tmpFile
